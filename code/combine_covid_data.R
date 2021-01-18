1e617d6937dacce08a5b5435123f267aa58f3f1f
Daniel Medina <danielmedinaneira555@gmail.com>
2021-01-17 22:14:08 -0500

    clean covid data

--- code/combine_covid_data.R
+++ code/combine_covid_data.R
@@ -1,11 +1,33 @@
 library(readr)
+library(dplyr)
+library(lubridate)
+
+# first, select the latest files from the folder with covid data
 
 files <- list.files("data/raw/covid_peru/", pattern = "fallecidos", full.names = TRUE)
 last_file <- files[length(files)]
-fallecidos <- read_csv(last_file)
+fallecidos <- read_csv2(last_file)
+fallecidos[,"FECHA_FALLECIMIENTO"] <- ymd(fallecidos[,"FECHA_FALLECIMIENTO"][[1]])
+# obtain just the month
+fallecidos[,"FECHA_FALLECIMIENTO"] <- month(fallecidos[,"FECHA_FALLECIMIENTO"][[1]])
 
 files <- list.files("data/raw/covid_peru/", pattern = "positivos", full.names = TRUE)
 last_file <- files[length(files)]
-positivos <- read_csv(last_file)
+positivos <- read_csv2(last_file)
+positivos[,"FECHA_RESULTADO"] <- ymd(positivos[,"FECHA_RESULTADO"][[1]])
+# obtain just the month
+positivos[,"FECHA_RESULTADO"] <- month(positivos[,"FECHA_RESULTADO"][[1]])
 
-# combine the data into one data set
+# pipes to save files
+
+fallecidos %>% 
+  rename(MES = FECHA_FALLECIMIENTO) %>% 
+  group_by(PROVINCIA, MES) %>% 
+  count() %>% 
+  write_csv("data/clean/fallecidos_covid.csv")
+
+positivos %>% 
+  rename(MES = FECHA_RESULTADO) %>% 
+  group_by(PROVINCIA, MES) %>% 
+  count() %>% 
+  write_csv("data/clean/positivos_covid.csv")
